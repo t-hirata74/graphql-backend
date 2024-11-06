f91eea23339bcdf8eae7c08532b55b2a47e0f304
t-hirata74 <t.hirata.0463@gmail.com>
2024-11-06 11:33:08 +0900

    query_typeで書いていた内容をリゾルバに移動

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -34,13 +34,15 @@ module Types
       Link.all
     end
 
-    field :user, Types::UserType, null: false do # GraphQLクエリでのfield名、GraphQL Type、必須かどうか
-      argument :id, ID, required: true # GraphQLクエリでの引数、GraphQLでの型、必須かどうか
-    end
+    ## Resolverに移動させる
+    field :user, resolver: Resolvers::UserResolver
+    # field :user, Types::UserType, null: false do # GraphQLクエリでのfield名、GraphQL Type、必須かどうか
+    #   argument :id, ID, required: true # GraphQLクエリでの引数、GraphQLでの型、必須かどうか
+    # end
     
-    def user(id:) # fieldが指定されたとき、どのようにデータ取得するかをfieldと同名メソッドで実装する
-      User.find(id)
-    end
+    # def user(id:) # fieldが指定されたとき、どのようにデータ取得するかをfieldと同名メソッドで実装する
+    #   User.find(id)
+    # end
 
     field :users, [Types::UserType], null: false
     
