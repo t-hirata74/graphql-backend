04c0339f46f1a7e8f2d78b03fdb89bd6236aad1a
t-hirata74 <t.hirata.0463@gmail.com>
2024-11-06 18:36:16 +0900

    Mutations::SignInUserのリゾルバ追加

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,6 +4,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
     field :create_link, mutation: Mutations::CreateLink
+    field :signin_user, mutation: Mutations::SignInUser
 
     # TODO: remove me
     field :test_field, String, null: false,
