949dd880e6c35534d11c984ca0b4c74a162810be
t-hirata74 <t.hirata.0463@gmail.com>
2024-11-06 18:03:02 +0900

    AuthProviderCredentialsInputの追加

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,17 +1,41 @@
-# frozen_string_literal: true
-
 module Mutations
   class CreateUser < BaseMutation
-    field :user, Types::UserType, null: true
+    # often we will need input types for specific mutation
+    # in those cases we can define those input types in the mutation class itself
+    class AuthProviderSignupData < Types::BaseInputObject
+      argument :credentials, Types::AuthProviderCredentialsInput, required: false
+    end
 
     argument :name, String, required: true
-    argument :email, String, required: false
+    argument :auth_provider, AuthProviderSignupData, required: false
 
-    def resolve(**args)
-      user = User.create!(args)
-      {
-        user: user
-      }
+    type Types::UserType
+
+    def resolve(name: nil, auth_provider: nil)
+      User.create!(
+        name: name,
+        email: auth_provider&.[](:credentials)&.[](:email),
+        password: auth_provider&.[](:credentials)&.[](:password)
+      )
     end
   end
 end
+
+# 実行時メモ
+# mutation {
+#   createUser(
+#     input: {
+#       name: "Test User",
+#       authProvider: {
+#         credentials: {
+#           email: "email@example.com",
+#           password: "123456"
+#         }
+#       }
+#     }
+#   ) {
+#     id
+#     name
+#     email
+#   }
+# }
