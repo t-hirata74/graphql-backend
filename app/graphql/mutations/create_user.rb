798f78168393f19000c5196e3ca7233627f02fc2
t-hirata74 <t.hirata.0463@gmail.com>
2024-11-06 11:15:50 +0900

    CreateUserの内容を更新

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -2,15 +2,16 @@
 
 module Mutations
   class CreateUser < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+    field :user, Types::UserType, null: true
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    argument :name, String, required: true
+    argument :email, String, required: false
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    def resolve(**args)
+      user = User.create!(args)
+      {
+        user: user
+      }
+    end
   end
 end
