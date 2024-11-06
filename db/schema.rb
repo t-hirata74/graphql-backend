a243e2d9c06617a333f812fc36815bbc17ace2d9
t-hirata74 <t.hirata.0463@gmail.com>
2024-11-06 14:35:35 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_06_023733) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_06_053505) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.string "url"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
