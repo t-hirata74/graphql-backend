bf5873400e6f2c1f7e985cc79359700ed6ed7dc1
t-hirata74 <t.hirata.0463@gmail.com>
2024-11-06 10:38:28 +0900

    query_typeにfieldを追加

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -34,5 +34,13 @@ module Types
       Link.all
     end
 
+    field :user, Types::UserType, null: false do # GraphQLクエリでのfield名、GraphQL Type、必須かどうか
+      argument :id, ID, required: true # GraphQLクエリでの引数、GraphQLでの型、必須かどうか
+    end
+    
+    def user(id:) # fieldが指定されたとき、どのようにデータ取得するかをfieldと同名メソッドで実装する
+      User.find(id)
+    end
+
   end
 end
