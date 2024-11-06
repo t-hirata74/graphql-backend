1a6d128c5e4e48c51f69e91cc3b6588ca05d08ea
t-hirata74 <t.hirata.0463@gmail.com>
2024-11-06 15:14:29 +0900

    コメント追記

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -28,8 +28,11 @@ module Types
       "Hello World!"
     end
 
-    field :all_links, [Types::LinkType], null: false
+    # queries are just represented as fields
+    # `all_links` is automatically camelcased to `allLinks`
+    field :all_links, [LinkType], null: false
 
+    # this method is invoked, when `all_link` fields is being resolved
     def all_links
       Link.all
     end
