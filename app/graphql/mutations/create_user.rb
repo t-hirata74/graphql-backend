19543d88f8cca36aa459f94e4b987e2aed9f20d6
t-hirata74 <t.hirata.0463@gmail.com>
2024-11-06 11:09:17 +0900

    bin/rails g graphql:mutation CreateUser

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Mutations
+  class CreateUser < BaseMutation
+    # TODO: define return fields
+    # field :post, Types::PostType, null: false
+
+    # TODO: define arguments
+    # argument :name, String, required: true
+
+    # TODO: define resolve method
+    # def resolve(name:)
+    #   { post: ... }
+    # end
+  end
+end
