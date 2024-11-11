7f83191e008832c9b39f474ee2b452a740e456a4
t-hirata74 <t.hirata.0463@gmail.com>
2024-11-11 23:01:45 +0900

    bundle exec rails generate model Vote link:references user:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_06_072858) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_11_140112) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "user_id", null: false
@@ -34,5 +34,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_06_072858) do
     t.string "password_digest"
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer "link_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_votes_on_link_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "books", "users"
+  add_foreign_key "votes", "links"
+  add_foreign_key "votes", "users"
 end
