191c8f47260fd905d035a1ae7211bc3002c7f9c7
t-hirata74 <t.hirata.0463@gmail.com>
2024-11-11 23:07:17 +0900

    rails db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_11_140112) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_11_140601) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "user_id", null: false
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_11_140112) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_11_140112) do
   end
 
   add_foreign_key "books", "users"
+  add_foreign_key "links", "users"
   add_foreign_key "votes", "links"
   add_foreign_key "votes", "users"
 end
